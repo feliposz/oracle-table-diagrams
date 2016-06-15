1dfa73a9e76c52a9533a06ed1f4e449f2b12109c
feliposz <feliposz@gmail.com>
2016-06-15 16:06:15 -0300

    Column relationships

--- tabgraph.sql
+++ tabgraph.sql
@@ -5,25 +5,32 @@ spool c:\temp\tmp_tabgraph.gv
 select 'digraph TabGraph {rankdir=LR;ratio=fill;node[shape=record];' from dual -- header
 union all
 -- relationships
-select distinct uc.table_name
-             || ' -> ' || ucc.table_name
+select distinct uc.table_name || ':<'||pkc.column_name||'>'
+             || ' -> ' || pkc.table_name || ':<'||fkc.column_name||'>'
              || ' [label="' || lower(uc.constraint_name) || '"];'
-from user_constraints uc, user_cons_columns ucc
-where uc.r_constraint_name = ucc.constraint_name
+from user_constraints uc, user_cons_columns pkc, user_cons_columns fkc
+where uc.r_constraint_name = pkc.constraint_name
+  and uc.r_constraint_name = fkc.constraint_name
   and uc.constraint_type = 'R'
   and regexp_like(uc.table_name, upper('&&1'))
+  and pkc.position = 1
+  and fkc.position = 1
 union all
 -- columns
 select ut.table_name
     || ' [label="' || ut.table_name || '|'
-    || listagg(lower(utc.column_name), '\n') within group (order by utc.column_id) || '"];'
-from user_tables ut, user_tab_columns utc
+    || listagg('<' || utc.column_name || '> ' || lower(utc.column_name), '|') within group (order by utc.column_id) || '"];'
+from user_tables ut, user_tab_columns utc, user_cons_columns ucc
 where ut.table_name = utc.table_name
+  and utc.table_name = ucc.table_name (+)
+  and utc.column_name = ucc.column_name (+)
+  and nvl(ucc.position, 1) = 1
   and regexp_like(utc.table_name, upper('&&1'))
 group by ut.table_name
 union all
 -- footer
 select '}' from dual;
+
 spool off
 
 -- reset after script
