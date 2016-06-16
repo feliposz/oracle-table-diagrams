c5f615c1dd840a0b878ac7216468dbfcf361be59
feliposz <feliposz@gmail.com>
2016-06-16 16:06:16 -0300

    Using HTML for formatting

--- tabgraph.sql
+++ tabgraph.sql
@@ -1,30 +1,42 @@
+prompt Generating GraphViz script...
+
 -- settings to build graphviz script (.gv)
-set timing off feedback off heading off termout off
+set timing off feedback off heading off termout off wrap on
 
 spool c:\temp\tmp_tabgraph.gv
-select 'digraph TabGraph {rankdir=LR;ratio=fill;node[shape=record];' from dual -- header
+select 'graph TabGraph {rankdir=LR;ratio=fill;edge[fontname=Consolas,fontsize=8];node[shape=record,fontname=Consolas,fontsize=8];' from dual -- header
 union all
 -- relationships
-select distinct uc.table_name || ':<'||pkc.column_name||'>'
-             || ' -> ' || pkc.table_name || ':<'||fkc.column_name||'>'
-             || ' [label="' || lower(uc.constraint_name) || '"];'
-from user_constraints uc, user_cons_columns pkc, user_cons_columns fkc
-where uc.r_constraint_name = pkc.constraint_name
-  and uc.r_constraint_name = fkc.constraint_name
+select distinct uc.table_name
+             || ' -- ' || ucc.table_name
+             || ' [dir=forward];'
+from user_constraints uc, user_cons_columns ucc
+where uc.r_constraint_name = ucc.constraint_name
   and uc.constraint_type = 'R'
   and regexp_like(uc.table_name, upper('&&1'))
-  and pkc.position = 1
-  and fkc.position = 1
 union all
 -- columns
 select ut.table_name
-    || ' [label="' || ut.table_name || '|'
-    || listagg('<' || utc.column_name || '> ' || lower(utc.column_name), '|') within group (order by utc.column_id) || '"];'
-from user_tables ut, user_tab_columns utc, user_cons_columns ucc
+    || ' [label=<<b>' || ut.table_name || '</b>|<table border="0">'
+    || listagg('<tr><td>' || nvl(k.keys, chr(38)||'nbsp;') || '</td>'
+               || '<td align="left">' || decode(utc.nullable,'N','<b>','')
+               || lower(utc.column_name) || decode(utc.nullable, 'N','</b>','') || '</td></tr>', chr(10))
+       within group (order by utc.column_id)
+    || '</table>>];'
+from user_tables ut
+   , user_tab_columns utc
+   , ( select owner
+            , table_name
+            , column_name
+            , listagg(regexp_replace(constraint_name, '.*_(PK|FK[0-9])$', '\1'), ',')
+              within group (order by replace(regexp_replace(constraint_name, '.*_(PK|FK[0-9])$', '\1'), 'PK', 'FK0')) as keys
+       from user_cons_columns
+       where regexp_like(constraint_name, '(_PK|_FK[0-9]+)$')
+       group by owner, table_name, column_name ) k
 where ut.table_name = utc.table_name
-  and utc.table_name = ucc.table_name (+)
-  and utc.column_name = ucc.column_name (+)
-  and nvl(ucc.position, 1) = 1
+  and utc.table_name = k.table_name (+)
+  and utc.column_name = k.column_name (+)
+  and nvl(k.owner, user) = user
   and regexp_like(utc.table_name, upper('&&1'))
 group by ut.table_name
 union all
@@ -34,10 +46,11 @@ select '}' from dual;
 spool off
 
 -- reset after script
-set timing on feedback on heading on termout on
+set timing on feedback on heading on termout on wrap off
 
+prompt Generating diagram
 -- generate .svg from script
-$V:\Portable\Programas\Graphviz\bin\dot.exe C:\Temp\tmp_tabgraph.gv -oC:\Temp\tmp_tabgraph.svg -Tsvg
+$V:\Portable\Programas\Graphviz\bin\dot.exe C:\Temp\tmp_tabgraph.gv -oC:\Temp\tmp_tabgraph.svg -Tsvg -Goverlap=false
 
 -- open in default browser/viewer
 $start C:\Temp\tmp_tabgraph.svg
